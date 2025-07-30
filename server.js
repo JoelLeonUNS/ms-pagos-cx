4c4150a0b1ad90e6354bc87b0935ea4c4f1359d2
JoelLeonUNS <202014033@uns.edu.pe>
2025-07-30 14:57:07 -0500

    Cors

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 require('dotenv').config();
 
+app.use(cors());
 app.use(express.json());
 
 const pagosRoutes = require('./routes/pagos');
@@ -13,8 +15,7 @@ app.use('/', mercadoPagoRoutes);
 const webhookRoutes = require('./routes/webhook');
 app.use('/', webhookRoutes);
 
-
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Microservicio de pagos activo en http://localhost:${PORT}`);
 });
